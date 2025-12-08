69ea75cfbb2fc35852e84ad56a00218a0e141ff7
Angel Patricio Lucero <alucerol1@est.ups.edu.ec>
2025-12-07 19:17:06 -0500

    boton  claro/oscuro

--- src/app/layouts/programador/programador-layout/programador-layout.ts
+++ src/app/layouts/programador/programador-layout/programador-layout.ts
@@ -2,16 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
 
-import { AuthService, UsuarioApp } from '../../../services/auth';                 // ✅
-import { NotificacionComponent } from '../../../components/notificacion/notificacion'; // ✅
+import { AuthService, UsuarioApp } from '../../../services/auth';                 
+import { NotificacionComponent } from '../../../components/notificacion/notificacion'; 
 import { Observable } from 'rxjs';
-
+import { ThemeToggleComponent } from '../../../components/theme-toggle/theme-toggle';
 @Component({
   selector: 'app-programador-layout',
   standalone: true,
   templateUrl: './programador-layout.html',
   styleUrls: ['./programador-layout.scss'],
-  imports: [CommonModule, RouterModule, NotificacionComponent]
+  imports: [CommonModule, RouterModule, NotificacionComponent, ThemeToggleComponent]
 })
 export class ProgramadorLayoutComponent {
 
--- src/app/layouts/public/public-layout/public-layout.ts
+++ src/app/layouts/public/public-layout/public-layout.ts
@@ -2,16 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
 
-import { AuthService, UsuarioApp } from '../../../services/auth';                 // ✅
-import { NotificacionComponent } from '../../../components/notificacion/notificacion'; // ✅
+import { AuthService, UsuarioApp } from '../../../services/auth';
+import { NotificacionComponent } from '../../../components/notificacion/notificacion';
 import { Observable } from 'rxjs';
-
+import { ThemeToggleComponent } from '../../../components/theme-toggle/theme-toggle';
 @Component({
   selector: 'app-public-layout',
   standalone: true,
   templateUrl: './public-layout.html',
   styleUrls: ['./public-layout.scss'],
-  imports: [CommonModule, RouterModule, NotificacionComponent]
+  imports: [CommonModule, RouterModule, NotificacionComponent, ThemeToggleComponent]
 })
 export class PublicLayoutComponent {
 
